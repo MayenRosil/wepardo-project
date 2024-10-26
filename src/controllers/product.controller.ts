4aa92ab5acfeaef69f017daa9741f484a3853426
Julio Mayén <mayenrosil@gmail.com>
2024-10-26 00:51:30 -0600

    nuevas funcionaclidades

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -22,3 +22,64 @@ export const getProducts = async (req: Request, res: Response) => {
     }
 
 }
+
+export const createProduct = async (req: Request, res: Response) => {
+    try{
+
+        const { image, name, points } = req.body;
+
+        await AppDataSource
+            .getRepository(Product)
+            .createQueryBuilder('product')
+            .insert()
+            .into(Product)
+            .values([
+                { image, name, points, active: true }
+            ])
+            .execute();
+
+        return res.json({ message: "Producto creado", errorCode: 0 });
+
+    } catch (error) {
+        if (error instanceof Error)
+            return res.status(500).json({ message: error.message, errorCode: 2 });
+    }
+
+}
+
+
+
+export const exchangeProduct = async (req: Request, res: Response) => {
+    try {
+        const { points, user } = req.body;
+
+        //Obtener el usuario a canjear
+        const findedUser = await AppDataSource
+        .getRepository(User)
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id: user })
+        .getOne();
+
+        if(!findedUser)  return res.json({ message: "Usuario no existe", errorCode: 1 });
+
+        if(findedUser.exchangePoints == 0)  return res.json({ message: "Usuario no cuenta con puntos suficientes", errorCode: 1 });
+
+        if(findedUser.exchangePoints < points)  return res.json({ message: "Usuario no cuenta con puntos suficientes", errorCode: 1 });
+
+        //Actualizar los puntos del usuario despues de canjear
+        await AppDataSource
+            .getRepository(User)
+            .createQueryBuilder('user')
+            .update(User)
+            .set({ exchangePoints: findedUser.exchangePoints - points })
+            .where("id = :id", { id: user })
+            .execute();
+
+        return res.json({ message: "Producto canejado, se restaron puntos del usuario", errorCode: 0 });
+
+    } catch (error) {
+        if (error instanceof Error)
+            return res.status(500).json({ message: error.message, errorCode: 2 });
+    }
+
+}
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getProducts } from '../controllers/product.controller';
+import { getProducts, createProduct, exchangeProduct } from '../controllers/product.controller';
 import { validateToken } from '../libs/validateToken';
 
 const router: Router = Router();
 
 router.get('/', validateToken, getProducts);
+router.post('/', validateToken, createProduct);
+router.patch('/', validateToken, exchangeProduct);
 
 export default router;
